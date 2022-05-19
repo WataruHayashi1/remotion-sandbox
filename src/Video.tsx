50b04c7e648b3490569ee2f709bb5645227f2756
WataruHayashi1 <wataru.hayashi@hokuden-it.co.jp>
2022-05-19 10:29:32 +0900

    springを追加

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,8 +1,9 @@
-import {Composition} from 'remotion';
-import {HelloWorld} from './HelloWorld';
-import {Logo} from './HelloWorld/Logo';
-import {Subtitle} from './HelloWorld/Subtitle';
-import {Title} from './HelloWorld/Title';
+import { Composition } from 'remotion';
+import { HelloWorld } from './HelloWorld';
+import { MyVideo } from './Sample/spring';
+import { Logo } from './HelloWorld/Logo';
+import { Subtitle } from './HelloWorld/Subtitle';
+import { Title } from './HelloWorld/Title';
 
 export const RemotionVideo: React.FC = () => {
 	return (
@@ -46,6 +47,18 @@ export const RemotionVideo: React.FC = () => {
 				fps={30}
 				width={1920}
 				height={1080}
+			/>
+			<Composition
+				id="Spring"
+				component={MyVideo}
+				durationInFrames={150}
+				fps={30}
+				width={1920}
+				height={1080}
+				defaultProps={{
+					titleText: 'Welcome to Remotion',
+					titleColor: 'black',
+				}}
 			/>
 		</>
 	);
